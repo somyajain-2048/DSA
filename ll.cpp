83ee0ec888a1152ae3145204f78dd20fb35735d8
somyajain-2048 <142176506+somyajain-2048@users.noreply.github.com>
2025-07-21 22:16:48 +0530

    Add files via upload

---
+++ ll.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;  
+            head = newNode;
+        }
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    List li;
+
+    li.push_front(3);
+    li.push_front(2);
+    li.push_front(1);
+
+    li.print();  
+    return 0;
+}
