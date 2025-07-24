e038e2fa0fdfb478586a6b67178820bc1fd3a18b
somyajain-2048 <142176506+somyajain-2048@users.noreply.github.com>
2025-07-24 23:30:59 +0530

    dsa learning

---
+++ middle.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    Node* getMiddle() {
+        Node* slow = head;
+        Node* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void printMiddle() {
+        Node* mid = getMiddle();
+        if (mid != NULL) {
+            cout << "Middle element: " << mid->data << endl;
+        } else {
+            cout << "List is empty.\n";
+        }
+    }
+};
+
+int main() {
+    List li;
+
+    li.insert(1);
+    li.insert(2);
+    li.insert(3);
+    li.insert(4);
+    li.insert(5);
+
+    li.print();         
+    li.printMiddle();  
+    return 0;
+}
