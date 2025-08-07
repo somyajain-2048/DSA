30675c898be2547f8e33bf21bd9e45926a6ee44c
somyajain-2048 <142176506+somyajain-2048@users.noreply.github.com>
2025-08-07 22:48:23 +0530

    stack implementation using linkedlist

--- stack.cpp
+++ stack.cpp
@@ -1,68 +1,136 @@
 
+// #include<iostream>
+// using namespace std;
+// class Stack{
+// 	int *arr;
+// 	int size;
+// int top;
+
+
+// public:
+// Stack(int s){
+// 	size  = s;
+// 	top = -1;
+// 	arr = new int[s];
+// }
+
+// void push(int value){
+// 	if(top == size-1){
+// 		cout<<"stack overflow";
+// 		return;
+// 	}
+// 	else{
+// 		top++;
+// 		arr[top]= value;
+// 		cout << "Pushed " << value << " into the stack\n";
+		
+		
+// 	}
+// }
+
+// void pop(){
+// 	if(top == -1){
+// 		cout<<"stack underflow";
+// 	}
+// 	else{
+		
+// 		cout << "Pop element from stack\n";
+// 		top--;
+// 	}
+// }
+// int peek(){
+// 	if(top == -1){
+// 		cout<<"stack is empty";
+// 		return -1;
+// 	}
+// 	else{
+// 		return arr[top];
+// 	}
+// }
+
+// bool isempty(){
+// 	return top == -1;
+// }
+
+// int isSize(){
+// 	return top+1;
+// }
+// };
+
 #include<iostream>
 using namespace std;
-class Stack{
-	int *arr;
+
+class Node{
+	public:
+	int data;
+	Node* next;
+
+	Node(int val){
+		data = val;
+		next = NULL;
+	}
+};
+
+class stack{
+	Node* top;
 	int size;
-int top;
 
-
-public:
-Stack(int s){
-	size  = s;
-	top = -1;
-	arr = new int[s];
-}
-
-void push(int value){
-	if(top == size-1){
-		cout<<"stack overflow";
-		return;
+	public:
+	stack(){
+  top = NULL;
+  size = 0;
 	}
-	else{
-		top++;
-		arr[top]= value;
-		cout << "Pushed " << value << " into the stack\n";
-		
-		
-	}
+
+void push(int val){
+	Node* newnode = new Node(val);
+	newnode->next = top;
+	top = newnode;
+	size++;
 }
 
 void pop(){
-	if(top == -1){
-		cout<<"stack underflow";
+	if(top == NULL){
+		cout << "Stack is empty.\n";
+		return;
 	}
 	else{
-		
-		cout << "Pop element from stack\n";
-		top--;
+		Node* temp = top;
+		top = top->next;
+    delete temp;
+		size--;
 	}
 }
+
 int peek(){
-	if(top == -1){
-		cout<<"stack is empty";
+	if(top == NULL){
+		 cout << "Stack is empty.\n";
 		return -1;
 	}
 	else{
-		return arr[top];
+		return top->data;
 	}
 }
 
 bool isempty(){
-	return top == -1;
+	return top == NULL;
 }
 
-int isSize(){
-	return top+1;
+int getSize(){
+	return size;
 }
 };
-
 int main(){
-Stack s(5);
+stack s;
 s.push(1);
-// s.push(2);
-s.pop();
-s.peek();
-s.isempty();
-s.isSize();
+s.push(2);
+
+
+    cout << "Top element: " << s.peek() << endl;
+    s.pop();
+    cout << "Top element after pop: " << s.peek() << endl;
+   
+		 cout << "Is stack empty? " << (s.isempty() ? "Yes" : "No") << endl;
+    cout << "Size of stack: " << s.getSize() << endl;
+
+    return 0;
 }
